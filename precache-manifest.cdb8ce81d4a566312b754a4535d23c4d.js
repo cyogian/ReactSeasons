6b36c3c7fe23ae1c1545a1afb27b72d230de62d4
Amar Nath Yogi <immortal2907@gmail.com>
2019-12-24 21:08:11 +0530

    Updates

---
+++ precache-manifest.cdb8ce81d4a566312b754a4535d23c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ba91696f1b1f339a054102c7906b8b",
+    "url": "/ReactSeasons/index.html"
+  },
+  {
+    "revision": "5bd90954701f1db27f53",
+    "url": "/ReactSeasons/static/css/main.e6f152ac.chunk.css"
+  },
+  {
+    "revision": "863993bdfe4d16efcdc8",
+    "url": "/ReactSeasons/static/js/2.6c9236af.chunk.js"
+  },
+  {
+    "revision": "5bd90954701f1db27f53",
+    "url": "/ReactSeasons/static/js/main.df90821f.chunk.js"
+  },
+  {
+    "revision": "549965779f3fd558a1c6",
+    "url": "/ReactSeasons/static/js/runtime~main.6c55e751.js"
+  }
+]);
